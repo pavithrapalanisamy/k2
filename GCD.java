ace261d0cd3ac4c08885a304735083519e40b1fc
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-06-24 11:04:16 +0530

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,29 @@
+package Guvi;
+import java.util.Scanner;
+public class GCD {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First no : ");
+        int n1=sc.nextInt();
+        System.out.print("Enter the Second no : ");
+        int n2=sc.nextInt();
+        int r;
+         
+        while(n2 != 0)
+        {
+            r = n1 % n2;
+            n1 = n2;
+            n2 = r;
+        }
+        System.out.print("GCD = "+n1);
+    }
+
+	}
+
+
+
+
+	
+
+
